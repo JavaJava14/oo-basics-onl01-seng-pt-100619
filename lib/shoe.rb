f6a9e2d3b7eaf9d62f43930087cc06f5b3c6e10f
Jose Chavira <joeychavira2016@gmail.com>
2019-10-20 00:58:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,8 @@
 class Shoe
-  attr_accessor :color, :size, :material
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   def initialize(brand)
     @brand = brand
   end
-  def condition
-
-  end
+  
 end
